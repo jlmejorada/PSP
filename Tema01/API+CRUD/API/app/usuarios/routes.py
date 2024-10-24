c79f1c24676ea32a9b871db3fdc47f1b51ea52a2
jlmejorada <joseluis.mejorada@iesnervion.es>
2024-10-24 12:58:32 +0200

    Actualización Token

--- Tema01/API+CRUD/API/app/usuarios/routes.py
+++ Tema01/API+CRUD/API/app/usuarios/routes.py
@@ -1,6 +1,23 @@
+import bcrypt
 from flask import Blueprint, request
+from flask_jwt_extended import create_access_token
 
+from app.ficheros.leer_escribir import leeFichero, escribeFichero
 
 ficheroUsuarios = "/app/ficheros/usuarios.json"
-
 usuariosBP = Blueprint('usarios', __name__)
+
+@usuariosBP.post('/')
+def registroUsuario():
+    usuarios = leeFichero(ficheroUsuarios)
+    if request.is_json:
+        nuevo_usuario = request.get_json()
+        contrasena = nuevo_usuario['Contrasena'].encode('utf-8')
+        salt = bcrypt.gensalt()
+        hashContrasena = bcrypt.hashpw(contrasena, salt).hex()
+        nuevo_usuario['Contrasena'] = hashContrasena
+        usuarios.append(nuevo_usuario)
+        escribeFichero(ficheroUsuarios, usuarios)
+        token = create_access_token(identity= nuevo_usuario ["Usuario"])
+        return {'token': token}, 201
+    return {"error": "Request must be JSON"}, 415
